3687ed91dc03d08518202da375f838b88679f85f
joebadmus <joebadmus@gmail.com>
2020-07-10 07:01:58 +0100

    Commented jenkins file

--- jenkins.tf
+++ jenkins.tf
@@ -1,43 +1,43 @@
-data "template_file" "jenkinsserver_data" {
-  template = "${file("template/jenkins.tpl")}"
-}
+# data "template_file" "jenkinsserver_data" {
+#   template = "${file("template/jenkins.tpl")}"
+# }
 
-resource "aws_instance" "jenkins_server" {
-  ami                         = var.ami
-  key_name                    = var.key_name
-  instance_type               = var.instance_type
-  user_data                   = data.template_file.jenkinsserver_data.rendered
-  subnet_id                   = aws_subnet.subnet.id
-  vpc_security_group_ids      = [aws_security_group.jenkins_server-sg.id]
-  associate_public_ip_address = true
+# resource "aws_instance" "jenkins_server" {
+#   ami                         = var.ami
+#   key_name                    = var.key_name
+#   instance_type               = var.instance_type
+#   user_data                   = data.template_file.jenkinsserver_data.rendered
+#   subnet_id                   = aws_subnet.subnet.id
+#   vpc_security_group_ids      = [aws_security_group.jenkins_server-sg.id]
+#   associate_public_ip_address = true
 
-  tags = {
-    Name = "jenkins_server"
-  }
-}
+#   tags = {
+#     Name = "jenkins_server"
+#   }
+# }
 
-resource "aws_security_group" "jenkins_server-sg" {
-  name   = "jenkins_server-security-group"
-  vpc_id = aws_vpc.main.id
+# resource "aws_security_group" "jenkins_server-sg" {
+#   name   = "jenkins_server-security-group"
+#   vpc_id = aws_vpc.main.id
 
-  ingress {
-    protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     protocol    = "tcp"
+#     from_port   = 22
+#     to_port     = 22
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    protocol    = "tcp"
-    from_port   = 8080
-    to_port     = 8080
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     protocol    = "tcp"
+#     from_port   = 8080
+#     to_port     = 8080
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    protocol    = -1
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     protocol    = -1
+#     from_port   = 0
+#     to_port     = 0
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
